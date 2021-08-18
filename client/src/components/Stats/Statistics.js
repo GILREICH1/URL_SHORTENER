5f3989867d93785a3b590e36498604a1634879b5
gilreich1 <gilreich12@gmail.com>
2021-08-18 18:39:08 +0100

    chore: cleanup display and CSS

--- client/src/components/Stats/Statistics.js
+++ client/src/components/Stats/Statistics.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 function Statistics() {
-  return <div>Statistics</div>;
+  const { shortURL } = useParams();
+  return (
+    <>
+      <h2>Statistics</h2>
+      <h3>{shortURL}</h3>
+    </>
+  );
 }
 
 export default Statistics;
