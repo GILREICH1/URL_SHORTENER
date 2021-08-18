f7522761d13ac76e18616bc942c4a98bc1cc9a8e
gilreich1 <gilreich12@gmail.com>
2021-08-18 18:05:37 +0100

    feat: ShortURL component displays

--- client/src/components/ShortURL/ShortURL.js
+++ client/src/components/ShortURL/ShortURL.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { base_url } from "../../utils/constants";
 
 function ShortURL({ shortURL }) {
-  return <p>{shortURL}</p>;
+  return (
+    <p>
+      You're short URL is {base_url}/short/{shortURL}
+    </p>
+  );
 }
 
 export default ShortURL;
