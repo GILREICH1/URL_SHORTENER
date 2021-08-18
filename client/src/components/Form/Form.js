652af047fad4c57c7a01d0992b2467ca12feb8c9
gilreich1 <gilreich12@gmail.com>
2021-08-18 18:02:49 +0100

    feat: integrating api call into form component

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -4,26 +4,28 @@ import ShortURL from "../ShortURL/ShortURL";
 
 function Form() {
   const [longURL, setLongURL] = useState("");
-  const [shortURL, setShortURL] = useState("");
+  const [newShortURL, setNewShortURL] = useState("");
 
   const submitHandler = async (e) => {
     e.preventDefault();
     const urlToSave = { longURL };
-    const { shortURL } = await generateShortURL(urlToSave);
-    setShortURL(shortURL);
+    const URLShortened = await generateShortURL(urlToSave);
+    setNewShortURL(URLShortened);
     setLongURL("");
   };
 
-  <form className="submitForm" onSubmit={(e) => submitHandler(e)}>
-    <input
-      className="form__input"
-      value={longURL}
-      type="text"
-      placeholder="paste in long URL here"
-      onChange={(e) => setLongURL(e.target.value)}
-    ></input>
-    {shortURL ? <ShortURL shortURL={shortURL} /> : null}
-  </form>;
+  return (
+    <form className="submitForm" onSubmit={(e) => submitHandler(e)}>
+      <input
+        className="form__input"
+        value={longURL}
+        type="text"
+        placeholder="paste in long URL here"
+        onChange={(e) => setLongURL(e.target.value)}
+      ></input>
+      {newShortURL ? <ShortURL shortURL={newShortURL} /> : null}
+    </form>
+  );
 }
 
 export default Form;
