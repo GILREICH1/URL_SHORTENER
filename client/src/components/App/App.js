a3a667e7ad4bcdce5f06e6d83fee0cba90982fc0
gilreich1 <gilreich12@gmail.com>
2021-08-18 18:37:01 +0100

    feat: redirection component

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Form from "../Form/Form";
 import Statistics from "../Stats/Statistics";
+import Redirector from "../Redirector/Redirector";
 import "./App.css";
 
 function App() {
   return (
     <Router>
-      <div>
+      <div className="App">
         <h1>URL Shortener</h1>
         <Switch>
-          <Route path="/stats">
-            <Statistics />
-          </Route>
+          <Route path="/stats/:shortURL" component={Statistics} />
+          <Route path="/short/:shortURL" component={Redirector} />
+          <Route exact path="/" component={Form} />
         </Switch>
       </div>
     </Router>
---
+++ client/src/components/Redirector/Redirector.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { getLongURL } from "../../utils/api-services";
+import { base_url } from "../../utils/constants";
+
+function Redirector() {
+  const { shortURL } = useParams();
+  console.log(shortURL);
+
+  useEffect(() => {
+    getLongURL(shortURL).then((longURL) => {
+      window.location.href = longURL;
+    });
+  });
+
+  return (
+    <p>
+      You're short URL is {base_url}/short/{shortURL}
+    </p>
+  );
+}
+
+export default Redirector;
