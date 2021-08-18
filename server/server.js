2ff02d5a426e09cfbae50a8da3ffeb78f43c0bba
gilreich1 <gilreich12@gmail.com>
2021-08-18 18:03:04 +0100

    feat: added cors

--- server/server.js
+++ server/server.js
@@ -2,9 +2,12 @@
 require("dotenv").config();
 const express = require("express");
 const router = require("./router");
+const cors = require("cors");
 require("./models/database");
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
