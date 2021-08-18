93a5ab6bbd32f1e963c7278d32c9c1d441b9128a
gilreich1 <gilreich12@gmail.com>
2021-08-18 18:36:20 +0100

    feat: tests for getURL api request

--- client/src/utils/api-services.test.js
+++ client/src/utils/api-services.test.js
@@ -1,4 +1,4 @@
-import { generateShortURL } from "./api-services";
+import { generateShortURL, getLongURL } from "./api-services";
 const longURL = "https://www.giffgaff.io/life/this-is-giffgaff";
 
 describe("generateShortURL", () => {
@@ -10,5 +10,16 @@ describe("generateShortURL", () => {
   it("should return a string of 6 characters", async () => {
     const response = await generateShortURL({ longURL });
     expect(response).toMatch(/^[A-Za-z0-9_-]{6,}$/);
+  });
+});
+
+describe("getURL", () => {
+  it("should retrieve a long URL from a short one", async () => {
+    // generate shortened URL
+    const newShortURL = await generateShortURL({ longURL });
+
+    // obtain long URL
+    const newLongURL = await getLongURL(newShortURL);
+    expect(newLongURL).toEqual(longURL);
   });
 });
