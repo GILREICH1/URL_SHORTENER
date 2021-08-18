e17e31225020848f777c83e690247c14b783e281
gilreich1 <gilreich12@gmail.com>
2021-08-18 19:07:50 +0100

    feat: increment visits from client

--- client/src/components/Redirector/Redirector.js
+++ client/src/components/Redirector/Redirector.js
@@ -1,11 +1,15 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { getLongURL } from "../../utils/api-services";
+import { getLongURL, incrementVisits } from "../../utils/api-services";
 import { base_url } from "../../utils/constants";
 
 function Redirector() {
   const { shortURL } = useParams();
   console.log(shortURL);
+
+  useEffect(() => {
+    incrementVisits(shortURL);
+  });
 
   useEffect(() => {
     getLongURL(shortURL).then((longURL) => {
--- client/src/utils/api-services.js
+++ client/src/utils/api-services.js
@@ -1,5 +1,6 @@
 import { base_url } from "./constants";
 
+// TODO MEANINGFUL ERROR HANDLING
 export const generateShortURL = async (newURL = {}) => {
   const jsonURL = JSON.stringify(newURL);
   const JSONresponse = await fetch(`${base_url}/saveURL`, {
@@ -22,6 +23,12 @@ export const getLongURL = async (shortURL) => {
 
 export const getURLStats = async (shortURL) => {
   const JSONresponse = await fetch(`${base_url}/getURL/${shortURL}`);
+  const response = await JSONresponse.json();
+  return response;
+};
+
+export const incrementVisits = async (shortURL) => {
+  const JSONresponse = await fetch(`${base_url}/incrementVisit/${shortURL}`);
   const response = await JSONresponse.json();
   return response;
 };
