4d5dfd274c873116778f18bea5e24beb73220433
gilreich1 <gilreich12@gmail.com>
2021-08-18 18:02:01 +0100

    feat: test generateShortURL returns 6 char string

--- client/src/utils/api-services.test.js
+++ client/src/utils/api-services.test.js
@@ -3,7 +3,12 @@ const longURL = "https://www.giffgaff.io/life/this-is-giffgaff";
 
 describe("generateShortURL", () => {
   it("should return a string", async () => {
-    const response = await generateShortURL(longURL);
+    const response = await generateShortURL({ longURL });
     expect(response).toEqual(expect.any(String));
+  });
+
+  it("should return a string of 6 characters", async () => {
+    const response = await generateShortURL({ longURL });
+    expect(response).toMatch(/^[A-Za-z0-9_-]{6,}$/);
   });
 });
