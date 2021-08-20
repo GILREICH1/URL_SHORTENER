6c055d88d5975871beb49709a5c42522ae2f73e8
gilreich1 <gilreich12@gmail.com>
2021-08-20 11:58:27 +0100

    feat: App SCSS

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -3,18 +3,20 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Form from "../Form/Form";
 import Statistics from "../Stats/Statistics";
 import Redirector from "../Redirector/Redirector";
-import "./App.css";
+import styles from "./App.module.scss";
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <h1>URL Shortener</h1>
-        <Switch>
-          <Route path="/stats/:shortURL" component={Statistics} />
-          <Route path="/short/:shortURL" component={Redirector} />
-          <Route exact path="/" component={Form} />
-        </Switch>
+      <div className={styles.App}>
+        <div>
+          <h1>URL Shortener</h1>
+          <Switch>
+            <Route path="/stats/:shortURL" component={Statistics} />
+            <Route path="/short/:shortURL" component={Redirector} />
+            <Route exact path="/" component={Form} />
+          </Switch>
+        </div>
       </div>
     </Router>
   );
